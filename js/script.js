0fc639ebc04b4bfd1940f6946e1a9560742111c5
samirankosambe <42654809+samirankosambe@users.noreply.github.com>
2020-07-09 14:05:22 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -119,7 +119,7 @@ function buildAndShowHomeHTML (categories) {
       //
       // var homeHtmlToInsertIntoMainPage = ....
 
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl,"randomCategoryShortName",chosenCategoryShortName)
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"randomCategoryShortName",chosenCategoryShortName)
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
